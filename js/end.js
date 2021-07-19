de486e8c9769a82039abfc9c04a805d78aefda25
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-07-18 20:32:58 -0400

    added to index.html some more text in an <h3>

--- js/end.js
+++ js/end.js
@@ -1,5 +1,3 @@
-// Constants-----------------------------------------------------//
-
 const initials = document.getElementById('initials');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
--- js/quiz.js
+++ js/quiz.js
@@ -101,6 +101,99 @@ let questions= [
 ];
 
 
+// Below FX is called at the end of this JS//
+function startQuiz() {
+    // Starting question count
+    questionCounter = 0;
+    // All questions are unused at the beginning
+    unusedQuestions = [ 
+        {
+            question: "What was the name of the ship Holden was serving on at the start of the show?",
+            choice1: "The Constellation",
+            choice2: "The Rocinante",
+            choice3: "The Exodus",
+            choice4: "The Canterbury",
+            answer: 4,
+        },
+        {
+            question: "Who is botanist of a soy farm on Ganymede Station?",
+            choice1: "Praxideke",
+            choice2: "Cotyar",
+            choice3: "Jefferson",
+            choice4: "Alex",
+            answer: 1,
+        },
+        {
+            question: "What was the name of Julie's racing ship?",
+            choice1: "Xuesen",
+            choice2: "Donnager",
+            choice3: "Razorback",
+            choice4: "Agatha King",
+            answer: 3,
+        },
+        {
+            question: "What is IO?",
+            choice1: "A moon around Jupiter",
+            choice2: "A maiden loved by Zues",
+            choice3: "One of the Galilean moons",
+            choice4: "All of the above",
+            answer: 4,
+        },
+        {
+            question: "How many traversable wormholes the Ring Network has?",
+            choice1: "123",
+            choice2: "1373",
+            choice3: "759",
+            choice4: "1743",
+            answer: 2,
+        },
+        {
+            question: "Where is Amos from?",
+            choice1: "Detroit",
+            choice2: "Baltimore",
+            choice3: "Melbourne",
+            choice4: "Chicago",
+            answer: 2,
+        },
+        {
+            question: "During the dramatic events of the 3rd season, Admiral Souther commanded the -?",
+            choice1: "Agatha King",
+            choice2: "Thomas Prince",
+            choice3: "Nathan Hale",
+            choice4: "Guanshiyin",
+            answer: 1,
+        },
+        {
+            question: "Which ship's captain is Sandrine Kirino?",
+            choice1: "Xuesen",
+            choice2: "Donnager",
+            choice3: "Hammurabi",
+            choice4: "Scirocco",
+            answer: 3,
+        },
+        {
+            question: "How many childen did Jules-Pierre Mao have?",
+            choice1: "2",
+            choice2: "3",
+            choice3: "5",
+            choice4: "7",
+            answer: 3,
+        },
+        {
+            question: "What was Drummer's job before she became a captain on the Behemoth?",
+            choice1: "XO on the Guy Molinari",
+            choice2: "Head of Belter security on Eros",
+            choice3: "Captain of the Toth",
+            choice4: "XO of the Canterbury",
+            answer: 1,
+        }
+    ];
+    //TEST: console.log(unusedQuestions);
+    //Function of getNewQuestion to follow
+    getNewQuestion();
+};
+
+
 // Function of getting each question-------------------------------------------------------//
 function getNewQuestion() {
 
@@ -119,16 +212,6 @@ function getNewQuestion() {
     // Below populates the question into the 'question' ID in the HTML
     question.innerText = currentQuestion.question;
 
-    // .forEach Method on MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach//
-    choices.forEach((choice) => {
-        const number = choice.dataset['number'];
-        // This will populate the choices into the "choice-option" in the html
-        choice.innerText = currentQuestion['choice' + number];
-    });
-    // This removes the old question and adds a new one//
-    // .splice method on MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice//
-    unusedQuestions.splice(questionIndex, 1);
-    acceptAnswers = true;
 };
 
 startQuiz();
