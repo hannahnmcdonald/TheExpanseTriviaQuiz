7e6dc5365cf75803f06fd3ca8f593fe55aa5a2fb
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-07-20 14:07:46 -0400

    attempting to fix 'cannot read property 'push' of null' error

--- js/end.js
+++ js/end.js
@@ -5,7 +5,7 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 // Below with the JSON.parse function, the highscores become numerical from a string. The opposite can be done from JSON.stringify.//
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse//
-const highScores = JSON.parse(localStorage.getItem('highScores'));
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 // Final score will be at the top
 finalScore.innerText = mostRecentScore; 
--- js/highscore.js
+++ js/highscore.js
@@ -3,7 +3,7 @@ const highScoresList = document.getElementById('highScoresList');
 // Below with the JSON.parse function, the highscores become numerical from a string. The opposite can be done from JSON.stringify.//
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse//
 
-const highScores = JSON.parse(localStorage.getItem("highScores")); 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || []; 
 
 //TEST: console.log(highScores);
 
